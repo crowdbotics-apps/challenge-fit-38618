f82963a1d4d3a1c6a8e688fc7f353cf71367163c
Crowdbotics <team@crowdbotics.com>
2023-02-01 00:40:39 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "challenge-fit-38618",
+  url: "https://challenge-fit-38618.botics.co",
+  api: "https://challenge-fit-38618.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
